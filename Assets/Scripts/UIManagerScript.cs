ff343c804a775345ae1e6d72a706166052ef5f1d
cafeeyen <s7070113@kmitl.ac.th>
2017-10-17 00:12:59 +0700

    0.0.1 - UI
    Add UI layout
    - Test scrollable menu
    - Test slide in-out menu

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public void ChangeBoolAnimator(Animator anim)
+    {
+        if(anim.GetBool("IsDisplayed") == true)
+            anim.SetBool("IsDisplayed", false);
+        else
+            anim.SetBool("IsDisplayed", true);
+    }
+}
