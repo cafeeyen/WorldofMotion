64bc2a661d40ea82dbf22f62b155b8988a733eba
cafeeyen <s7070113@kmitl.ac.th>
2017-10-28 19:29:14 +0700

    0.1.0 - Camera Controller
    Use touch gesture to control camera.
    *** Using TouchScript from asset store
    https://www.assetstore.unity3d.com/en/#!/content/7394 ***
    - Drag with 1 finger to move.
    - Drag with 2 fingers to rotate.
    - Rotate with 2 fingers to rotate around Z axis.
    - Pinch to zoom.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using TouchScript.Gestures.TransformGestures;
+
+// Base on CameraController.cs in Camera Example from TouchScript asset
+
+public class CameraController : MonoBehaviour
+{
+    public Transform pivot;
+    public Transform cam;
+    public ScreenTransformGesture MoveGesture;
+    public ScreenTransformGesture MultiTouchGesture;
+
+    public float moveSpeed = 0.1f;
+    public float rotateSpeed = 100f;
+    public float zoomSpeed = 50f;
+
+    private void OnEnable()
+    {
+        MoveGesture.Transformed += moveHandler;
+        MultiTouchGesture.Transformed += multiTouchHandler;
+    }
+
+    private void OnDisable()
+    {
+        MoveGesture.Transformed -= moveHandler;
+        MultiTouchGesture.Transformed -= multiTouchHandler;
+    } 
+
+    private void moveHandler(object sender, System.EventArgs e)
+    {
+        // Move camera
+        pivot.localPosition += cam.rotation * MoveGesture.DeltaPosition * -moveSpeed;
+    }
+
+    private void multiTouchHandler(object sender, System.EventArgs e)
+    {
+        // Rotate camera
+        Quaternion rotation = Quaternion.Euler(-MultiTouchGesture.DeltaPosition.y/Screen.height*rotateSpeed,
+            MultiTouchGesture.DeltaPosition.x/Screen.width*rotateSpeed,
+            MultiTouchGesture.DeltaRotation);
+        pivot.localRotation *= rotation;
+
+        // Zoom camera
+        cam.localPosition += Vector3.forward * (MultiTouchGesture.DeltaScale - 1f) * zoomSpeed;
+    }
+
+}
