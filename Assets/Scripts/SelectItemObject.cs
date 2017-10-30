0fb14db1f5ba134f07efa0e9d0f1c29549551c27
cafeeyen <s7070113@kmitl.ac.th>
2017-10-31 00:26:27 +0700

    0.2.0 - ItemObject (Control with touch)
    - ItemObject(Gameobject) move 1 unit in app for every 10 unit in unity.
    - Tap ItemObject to select. Selected ItemObject will have growing
    effect.
    - After select, drag to ItemObject move.

---
+++ Assets/Scripts/SelectItemObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TouchScript.Gestures;
+
+public class SelectItemObject : MonoBehaviour 
+{
+    public GameObject growEffect;
+
+    private bool state = false;
+    private TapGesture gesture;
+
+    private void OnEnable()
+    {
+        growEffect.SetActive(false);
+        gesture = GetComponent<TapGesture>();
+        gesture.Tapped += tapHandler;
+    }
+
+    private void OnDisable()
+    {
+        gesture.Tapped -= tapHandler;
+    }
+
+    private void tapHandler(object sender, System.EventArgs e)
+    {
+        state = true;
+        growEffect.SetActive(true);
+        GetComponent<DragNDrop>().enabled = true;
+    }
+}
