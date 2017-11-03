d990cab99c59eda82a989b7f34a7df8a7890ef85
cafeeyen <s7070113@kmitl.ac.th>
2017-11-03 22:30:32 +0700

    0.2.1 - Drag and drop ItemObject by axis
    - When select item, axis will appear as cylinder with color base on
    axis.
    - Touch and drag cylinder to move only in that axis.

---
+++ Assets/Scripts/ItemObject/DragNDrop.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TouchScript.Gestures.TransformGestures;
+
+public class DragNDrop : MonoBehaviour
+{
+    private TransformGesture gesture;
+    private Vector3 curPos;
+    private GameObject axisTransition;
+
+    private void OnEnable()
+    {
+        gesture = GetComponent<TransformGesture>();
+        curPos = transform.localPosition;
+        gesture.Transformed += transfromMoveHandler;
+    }
+
+    private void OnDisable()
+    {
+        gesture.Transformed -= transfromMoveHandler;
+    }
+
+    private void transfromMoveHandler(object sender, System.EventArgs e)
+    {
+        curPos += transform.rotation * gesture.DeltaPosition;
+        // Set block 1(app):10(Unity)
+        transform.localPosition = new Vector3(Mathf.Round(curPos.x / 10) * 10, Mathf.Round(curPos.y / 10) * 10, Mathf.Round(curPos.z / 10) * 10);
+        axisTransition.GetComponent<AxisTransition>().updatePosition(transform.localPosition);
+    }
+
+    public void setAxisTransition(GameObject axis)
+    {
+        axisTransition = axis;
+    }
+
+    public void updatePosition(Vector3 pos)
+    {
+        // Update position from axis
+        transform.localPosition = pos;
+        curPos = pos;
+    }
+}
