7b626cb042c06c1b30554cbb2317525bd49f3179
cafeeyen <s7070113@kmitl.ac.th>
2017-11-12 15:36:06 +0700

    0.2.4 Add function to properties window
    - Can open properties window after select ItemObject.
    - Show postion on realtime.
    - Use slider to change scale.
    - Can rotate 30/45 degree(select only one) clockwise/anti-clockwise.

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using TouchScript.Gestures;
+
+public class UIController : MonoBehaviour
+{
+    public Animator menu, item, prop;
+
+    private GameObject selectedItemObject;
+    private TapGesture gesture;
+
+    private void OnEnable()
+    {
+        gesture = GetComponent<TapGesture>();
+        gesture.Tapped += tapHandler;
+    }
+
+    private void OnDisable()
+    {
+        gesture.Tapped -= tapHandler;
+    }
+
+    private void tapHandler(object sender, System.EventArgs e)
+    {
+        string tapped = gesture.GetScreenPositionHitData().Target.name;
+        if(tapped == "MenuButton" || tapped == "MenuArrow") displayWindows(menu);
+        else if(tapped == "ItemButton" || tapped == "ItemArrow") displayWindows(item);
+        else if(tapped == "PropButton" || tapped == "PropArrow") displayWindows(prop);
+    }
+
+    public void displayWindows(Animator anim)
+    {
+        if (anim.GetBool("IsDisplayed") == true)
+            anim.SetBool("IsDisplayed", false);
+        // PropBar can open when some ItemObject has selected
+        else if(anim.name != "PropBar" || selectedItemObject != null)
+        {
+            anim.SetBool("IsDisplayed", true);
+            changeState(anim);
+        }
+    }
+	private void changeState(Animator anim)
+    {
+        // Can open one window at the same time
+        if(anim == menu)
+        {
+            item.SetBool("IsDisplayed", false);
+            prop.SetBool("IsDisplayed", false);
+        }
+        else if(anim == item)
+        {
+            menu.SetBool("IsDisplayed", false);
+            prop.SetBool("IsDisplayed", false);
+        }
+        else // anim == prop
+        {
+            menu.SetBool("IsDisplayed", false);
+            item.SetBool("IsDisplayed", false);
+        }
+    }
+
+    public void setSelectedItemObject(GameObject itemObject)
+    {
+        selectedItemObject = itemObject;
+    }
+}
