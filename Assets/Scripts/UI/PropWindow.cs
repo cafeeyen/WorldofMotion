d182c27d244b52e13599de95c8f8ff17c3132d0b
cafeeyen <s7070113@kmitl.ac.th>
2017-12-06 14:34:33 +0700

    Change item scale
    - Change scale from 1:10 to 1:1.
    - Spawn possition distance decrease from 80 to 10.

--- Assets/Scripts/UI/PropWindow.cs
+++ Assets/Scripts/UI/PropWindow.cs
@@ -104,9 +104,9 @@ public class PropWindow : MonoBehaviour
         {
             // Start change state so slider won't change itemObject scale while set value from new itemObject
             changeState = true;
-            sliderX.value = itemObject.transform.localScale.x / 10;
-            sliderY.value = itemObject.transform.localScale.y / 10;
-            sliderZ.value = itemObject.transform.localScale.z / 10;
+            sliderX.value = itemObject.transform.localScale.x;
+            sliderY.value = itemObject.transform.localScale.y;
+            sliderZ.value = itemObject.transform.localScale.z;
 
             ItemObject itemObjectSc = itemObject.GetComponent<ItemObject>();
             selectedType = itemObjectSc.getSurType().getName();
@@ -139,7 +139,7 @@ public class PropWindow : MonoBehaviour
         {
             if (!changeState)
             {
-                itemObject.transform.localScale = new Vector3(sliderX.value, sliderY.value, sliderZ.value) * 10;
+                itemObject.transform.localScale = new Vector3(sliderX.value, sliderY.value, sliderZ.value);
                 itemObject.GetComponent<ItemObject>().checkCollider();
             }
                 
