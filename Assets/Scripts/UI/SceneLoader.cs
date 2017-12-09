ac0a28594d5644a6f20549b6f0517b359f0323d7
cafeeyen <s7070113@kmitl.ac.th>
2017-12-09 16:33:33 +0700

    0.3.2.0 Add main page and fix bug - Add main page(Title page) only NEW button works to load another scene. - In editor mode change import button to save. - In editor mode Mainmenu Button send bake to main page. Using this not save file. - Fix create item spawn point.

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject loadingPrefeb;
+
+    private Text loadingText;
+    private bool loadScene = false;
+    private float alpha;
+
+    public void loadNewScene(int sceneIndex, int saveIndex=0)
+    {
+        if(!loadScene)
+        {
+            loadScene = true;
+            loadingPrefeb.SetActive(true);
+            loadingText = loadingPrefeb.GetComponentInChildren<Text>();
+            if(saveIndex != 0)
+            {
+                // Do something
+            }
+            StartCoroutine(LoadScene(sceneIndex));
+            loadScene = false;
+        }
+    }
+
+    private IEnumerator LoadScene(int sceneIndex)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneIndex);
+
+        // While loading
+        while (!async.isDone)
+        {
+            alpha = Mathf.PingPong(Time.unscaledTime, 1.0f);
+            loadingText.color = loadingText.color * alpha;
+            yield return null;
+        }
+    }
+}
