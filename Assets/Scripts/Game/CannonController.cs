18e9650396a84be8c24d8a787be924b222eaf46f
cafeeyen <s7070113@kmitl.ac.th>
2017-12-30 22:54:18 +0700

    Update cannon ball effect, hit feedback, cancle cutscene
    - Add flame trail to cannon ball.
    - Change target size.
    - Cutscene change to show in mini image instead of arc line.
    -> Only if target detected.
    -> After some condition, mini image change back to arc line.
    - Show hit text after cannon ball hit target.
    - Change ball camera transfrom and zome a little when ball near target.

--- Assets/Scripts/Game/CannonController.cs
+++ Assets/Scripts/Game/CannonController.cs
@@ -7,24 +7,26 @@ public class CannonController : MonoBehaviour
     public TapGesture angleUp, angleDown, cannon;
     public Text angleText, heightText, timeText, disText;
     public InputField powerText;
-    public GameObject cannonBall, bigImage, overlay, Target, shootedTarget;
-    public Camera sideCam;
-    public Camera ballCam;
+    public GameObject cannonBall, bigImage, overlay, target, shootedTarget;
+    public Camera sideCam, ballCam;
     public LineRenderer arcLine, groundLine;
-    public RenderTexture sideCamRT;
+    public RenderTexture sideCamRT, cutCamRT;
     public RawImage rawImage, miniRawImage;
     public SceneLoader sceneLoader;
 
     private float maxHeight = 0, maxDist = 0, maxTime = 0;
     private float angle = 0, height = 0, power = 0;
     private float step;
-    private Vector3 cannonPos = new Vector3(0, -1.8f, 5.5f);
+    private Vector3 cannonPos = new Vector3(0, -1.8f, 5.5f), ballCamOffset = new Vector3(0, 2.8f, -5.5f);
+    private bool shooted = false;
 
     private void OnEnable()
     {
         step = Time.fixedDeltaTime * 1f;
         sideCamRT.width = (int)rawImage.rectTransform.rect.width;
         sideCamRT.height = (int)rawImage.rectTransform.rect.height;
+        cutCamRT.width = sideCamRT.width;
+        cutCamRT.height = sideCamRT.height;
         cannon.Tapped += ShootCannon;
     }
 
@@ -35,66 +37,80 @@ public class CannonController : MonoBehaviour
 
     void FixedUpdate()
     {
-        angle = transform.eulerAngles.x;
-        angle = angle > 180 ? angle - 360 : angle;
-
-        if (angleUp.State == Gesture.GestureState.Possible && angle > -90)
-            angle -= 1;
-        else if (angleDown.State == Gesture.GestureState.Possible && angle < 0)
-            angle += 1;
-
-        angle = Mathf.Clamp(angle, -90, 0);
-
-        transform.localRotation = Quaternion.Euler(angle, 0, 0);
-        angleText.text = string.Format("{0} \u00B0", Mathf.Round(-angle));
-        heightText.text = string.Format("Max height : {0} m.", System.Math.Round(maxHeight,2));
-        timeText.text = string.Format("Time of flight : {0} s.", System.Math.Round(maxTime,2));
-        disText.text = string.Format("Max distance : {0} m.", System.Math.Round(maxDist, 2));
-
-        if (string.IsNullOrEmpty(powerText.text))
+        if(!shooted)
         {
-            power = 0;
-        }
-        else
-        {
-            try
+            angle = transform.eulerAngles.x;
+            angle = angle > 180 ? angle - 360 : angle;
+
+            if (angleUp.State == Gesture.GestureState.Possible && angle > -90)
+                angle -= 1;
+            else if (angleDown.State == Gesture.GestureState.Possible && angle < 0)
+                angle += 1;
+
+            angle = Mathf.Clamp(angle, -90, 0);
+            transform.localRotation = Quaternion.Euler(angle, 0, 0);
+
+            angleText.text = string.Format("{0} \u00B0", Mathf.Round(-angle));
+            heightText.text = string.Format("Max height : {0} m.", System.Math.Round(maxHeight, 2));
+            timeText.text = string.Format("Time of flight : {0} s.", System.Math.Round(maxTime, 2));
+            disText.text = string.Format("Max distance : {0} m.", System.Math.Round(maxDist, 2));
+
+            if (string.IsNullOrEmpty(powerText.text))
             {
-                // Power min = 0, max = 30 (m/s)
-                var clamp = Mathf.Clamp(int.Parse(powerText.text), 0, 30);
-                power = int.Parse(powerText.text);
-                powerText.text = clamp.ToString();
+                power = 0;
             }
-            catch { }
-        }
-        
-        if (cannonBall.transform.position.z > Target.transform.position.z+2 || cannonBall.transform.position.y < -1.8)
-        {
-            //swap camera
-            ballCam.depth = -12;
-        }
-        if (Target.transform.position.z - cannonBall.transform.position.z < 2 && Target.transform.position.z - cannonBall.transform.position.z >= 0)
-        {
-            //slow down if close to the target and after pass target to normal speed
-            Time.timeScale = 0.08f;
-            Time.fixedDeltaTime = Time.timeScale * .02f;
+            else
+            {
+                try
+                {
+                    // Power min = 0, max = 30 (m/s)
+                    var clamp = Mathf.Clamp(int.Parse(powerText.text), 0, 30);
+                    power = int.Parse(powerText.text);
+                    powerText.text = clamp.ToString();
+                }
+                catch { }
+            }
         }
         else
         {
-            Time.timeScale = 1f;
-            Time.fixedDeltaTime = step;
+            ballCam.transform.position = cannonBall.transform.position + ballCamOffset;
+
+            var xDif = Mathf.Abs(shootedTarget.transform.position.x - cannonBall.transform.position.x);
+            var zDif = Mathf.Abs(shootedTarget.transform.position.z - cannonBall.transform.position.z);
+
+            if (Vector3.Distance(cannonBall.transform.position, shootedTarget.transform.position) < 2)
+            {
+                //Slow down if close to the target and after pass target to normal speed
+                Time.timeScale = 0.08f;
+                Time.fixedDeltaTime = Time.timeScale * .02f; // for smooth
+                ballCam.transform.position = cannonBall.transform.position + ballCamOffset + new Vector3(0, -1, 2 - zDif);
+            }
+            else if ((Time.timeScale == 0.08f || xDif > 5) || shootedTarget.transform.position.z - cannonBall.transform.position.z < -2 || cannonBall.transform.position.y < -1.8)
+            {
+                shooted = false;
+                ballCam.depth = -2;
+                Time.timeScale = 1f;
+                Time.fixedDeltaTime = step;
+                drawCurve();
+            }
         }
+
+        cannonBall.transform.LookAt(-cannonBall.GetComponent<Rigidbody>().velocity);
+
+        if(cannonBall.transform.position.y <= -50)
+            // We don't want ball position to limitbreak~
+            resetBall();
     }
 
     private void ShootCannon(object sender, System.EventArgs e)
     {
-        ballCam.depth = 1;
-        ballCam.enabled = true;
-        // Reset current force and position(reuse ball)
-        cannonBall.GetComponent<Rigidbody>().isKinematic = true;
-        cannonBall.transform.position = cannonPos;
-        cannonBall.transform.localEulerAngles = Vector3.zero;
-        cannonBall.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        if (!miniRawImage.enabled)
+            miniRawImage.enabled = true;
 
+        // Reset ball camera depth(cutsceneCam)
+        ballCam.depth = -2;
+        
+        resetBall();
         cannonBall.GetComponent<Rigidbody>().isKinematic = false;
         cannonBall.GetComponent<Rigidbody>().AddForce(transform.forward * power, ForceMode.Impulse);
 
@@ -147,17 +163,39 @@ public class CannonController : MonoBehaviour
             sideCam.orthographicSize = Mathf.Max(1, maxHeight * 0.9f);
         }
 
-        if(Target.GetComponent<TargetDetail>().Detected)
-            shootedTarget.transform.position = Target.transform.position;
+        if(target.GetComponent<TargetDetail>().Detected)
+        {
+            //Swap camera
+            ballCam.depth = 2;
+            ballCam.transform.position = ballCamOffset + cannonPos;
+            //ballCam.transform.LookAt(target.transform);
 
-        drawCurve();
+            // Show shooted target in side camera
+            shootedTarget.transform.position = target.transform.position;
+
+            miniRawImage.texture = cutCamRT;
+            rawImage.texture = cutCamRT;
+
+            shooted = true;
+        }
+        else
+            drawCurve();
+    }
+
+    private void resetBall()
+    {
+        // Reset current force and position(reuse ball)
+        cannonBall.GetComponent<Rigidbody>().isKinematic = true;
+        cannonBall.transform.position = cannonPos;
+        cannonBall.transform.localEulerAngles = Vector3.zero;
+        cannonBall.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 
     private void drawCurve()
     {
         // Y-1.8f and Z+5.5f from cannon offset
-        if (!miniRawImage.enabled)
-            miniRawImage.enabled = true;
+        miniRawImage.texture = sideCamRT;
+        rawImage.texture = sideCamRT;
 
         int maxIndex = Mathf.RoundToInt(maxTime / step);
         arcLine.positionCount = maxIndex;
--- Assets/Scripts/Game/TargetController.cs
+++ Assets/Scripts/Game/TargetController.cs
@@ -1,12 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetController : MonoBehaviour
 {
+    public Animator hitText;
+
+    private void OnEnable()
+    {
+        hitText.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Screen.height/2);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "CannonBall")
         {
-
+            StartCoroutine(showHitText());
         }
+    }
+
+    IEnumerator showHitText()
+    {
+        hitText.gameObject.GetComponent<Image>().enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        hitText.SetBool("Hit", true);
+        yield return new WaitForSeconds(1);
+        hitText.SetBool("Hit", false);
+        yield return new WaitForSeconds(0.2f);
+        hitText.gameObject.GetComponent<Image>().enabled = false;
     }
 }
