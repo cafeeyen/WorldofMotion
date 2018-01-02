019ae68439d200216c5503d71be4e1e2971736e8
Sathika Limawiruchaphong <s7070126@kmitl.ac.th>
2018-01-02 22:32:00 +0700

    Add tutorial page
    - add tutorial in cs
    - can exit/change page /move the dot

--- Assets/Scripts/UI/HelpButton.cs
+++ Assets/Scripts/UI/HelpButton.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HelpButton : MonoBehaviour
 {
     public GameObject overlay, helpSection;
+    public Sprite[] gallery; //store all tutorial image
+    public Image[] dot; //store dot that apear on screen
+    public Sprite colorDot; //store color dot image
+    public Sprite blackDot; //store black dot image
+    public Image displayImage; //The current image thats visible
+    private int i = 0;
+    private int j = 0;//check if it is nextpage or backpage
 
     public void openHelp()
     {
@@ -14,5 +22,42 @@ public class HelpButton : MonoBehaviour
     {
         overlay.SetActive(false);
         helpSection.SetActive(false);
+    }
+
+    public void nextImage()
+    {
+        if (i + 1 < gallery.Length)
+        {
+            i++;
+            j = 0;
+        }
+    }
+
+    public void PrevImage()
+    {
+        if (i - 1 >= 0)
+        {
+            i--;
+            j = 1;
+        }
+    }
+
+    void Update()
+    {
+        displayImage.sprite = gallery[i];
+        if (i  >= 1 && j==0)
+        {
+            dot[i].sprite = colorDot ;
+            dot[i - 1].sprite = blackDot;
+        }
+        else if (i >= 0 && j==1)
+        {
+            dot[i].sprite = colorDot;
+            dot[i + 1].sprite = blackDot;
+        }
+        else
+        {
+            dot[i].sprite = colorDot;
+        }
     }
 }
