c4a492c7eb9301e3a331d30bdead70c47aa5b10d
cafeeyen <s7070113@kmitl.ac.th>
2018-02-24 23:08:06 +0700

    Change mass base on scale

--- Assets/Scripts/ItemObject/ItemObject.cs
+++ Assets/Scripts/ItemObject/ItemObject.cs
@@ -5,7 +5,6 @@ using System.Linq;
 public class ItemObject : MonoBehaviour // Subject for ItemObjectController
 {
     private ItemObjectController ItemCon;
-    private int mass;
     private bool overlapping;
     private Renderer baseRenderer;
     private Material baseMat;
@@ -110,7 +109,6 @@ public class ItemObject : MonoBehaviour // Subject for ItemObjectController
     public PhysicMaterial PhyMat { get { return GetComponent<Collider>().material; } set { GetComponent<Collider>().material = value; } }
     public Renderer BaseRenderer { get { return baseRenderer; } set { baseRenderer = value; } }
     public bool IsOverlap { get { return overlapping; } }
-    public float Mass { get { return rb.mass; } }
     public Vector3 Velocity { get { return velocity; } set { velocity = value; rb.velocity = value; } }
     public string ItemType { get { return itemType; } set { itemType = value; } }
 }
